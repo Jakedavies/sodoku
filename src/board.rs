3929fd703e8da4e15302d219de87cbd7de63059c
Jake Davies <jgdavies24@gmail.com>
2022-07-02 09:22:16 +1200

    Generator backtracking, constraint bug

--- src/board.rs
+++ src/board.rs
@@ -1,28 +1,56 @@
-
 pub struct Board {
-    state: [[u8; 9]; 9],
+    state: [[Option<u8>; 9]; 9],
 }
 
 impl Board {
     pub fn new() -> Board {
         Self {
-            state: [[0; 9]; 9]
+            state: [[None; 9]; 9]
         }
     }
     pub fn set_tile(&mut self, x: usize, y: usize, value: u8) {
         println!("Setting tile at ({}, {}) to {}", x, y, value);
-        self.state[x][y] = value;
+        self.state[x][y] = Some(value);
+    }
+
+    pub fn clear_tile(&mut self, x: usize, y: usize) {
+        self.state[x][y] = None;
+    }
+
+    pub fn get_tile(&self, x: usize, y: usize) -> Option<u8> {
+        return self.state[x][y];
+    }
+
+    pub fn print(&self) {
+        for i in self.state {
+           println!("");
+           print!(" ");
+           for j in i {
+                if let Some(n) = j {
+                print!("{} ", n);
+               } else {
+                print!( "? ");
+               }
+           } 
+        }
     }
 
     pub fn validate(&self) -> bool {
+        for i in 0..9 {
+            for j in 0..9 {
+                if let None = self.state[i][j] {
+                    return false;
+                } 
+            }
+        }
         // check sodoku board is valid
         // check rows
         for i in 0..9 {
             let mut sum_1 = 0;
             let mut sum_2 = 0;
             for j in 0..9 {
-                sum_1 += self.state[i][j];
-                sum_2 += self.state[j][i];
+                sum_1 += self.state[i][j].unwrap();
+                sum_2 += self.state[j][i].unwrap();
             }
             if sum_1 != 45 || sum_2 != 45 {
                 return false;
@@ -34,7 +62,7 @@ impl Board {
                 let mut sum = 0;
                 for k in i*3..(i+1) * 3 {
                     for l in j*3..(j+1) * 3 {
-                        sum += self.state[k][l];
+                        sum += self.state[k][l].unwrap();
                     }
                 }
                 if sum != 45 {
--- src/board_generator.rs
+++ src/board_generator.rs
@@ -1,3 +1,4 @@
+#![feature(array_map)]
 use super::board::Board;
 use rand::prelude::*;
 use rand_chacha::rand_core::RngCore;
@@ -31,81 +32,121 @@ impl Default for GenerationParams {
     }
 }
 
-#[derive(Debug, Copy, Clone)]
+
+
+#[derive(Debug, Clone)]
 struct TileGenerationState {
-    candidate_count: u8,
-    candidates: [u8; 9],
+    candidates: Vec<u8>,
+    side_effects: Vec<usize>,
 }
+
 
 impl TileGenerationState {
     pub fn new() -> TileGenerationState {
         TileGenerationState {
-            candidates: [1,2,3,4,5,6,7,8,9],
-            candidate_count: 9,
+            candidates: vec![1,2,3,4,5,6,7,8,9],
+            side_effects: vec![],
         }
     }
 
-    pub fn get_candidates(&mut self, index: usize) -> &[u8] {
-        &self.candidates[0..self.candidate_count as usize]
+    pub fn get_candidates(&self) -> &Vec<u8> {
+        &self.candidates
     }
 
-    pub fn remove_candidate_value(&mut self, value: u8) {
-        for i in 0..self.candidate_count as usize {
-            if self.candidates[i] == value {
-                self.candidates[i] = self.candidates[self.candidate_count as usize - 1];
-                self.candidate_count -= 1;
-                break;
-            }
+    pub fn remove_candidate_value(&mut self, value: u8) -> bool {
+        let index = self.candidates.iter().position(|f| *f == value);
+        if let Some(index) = index {
+            self.candidates.swap_remove(index);
+            return true
         }
+        false
     }
 
-    pub fn elect_candidate_at_index(&mut self, index: usize) -> u8{
-        self.candidates[0] = self.candidates[index as usize];
-        self.candidate_count = 1;
-        self.candidates[0]
+    pub fn add_side_effect(&mut self, p: usize) {
+       self.side_effects.push(p);
     }
 }
 
+// if we have available candidates, pick one
+// if we do not, go back to previous node and remove the previously chosen candidate from the available pool. 
+// for every number at every position, we represent its "constraint" space using a 64 bit number. 9
+// bits for 
+//
+
+fn pointer_to_x_y(p: usize) -> (usize, usize) {
+    return (p / 9, p % 9)
+}
+ 
 
 pub fn generate_board(params: GenerationParams) -> Board {
     let mut board = Board::new();
     let mut rng: Pcg64 = Seeder::from(params.seed).make_rng();
     
-    let mut candidate_board: [[TileGenerationState; 9]; 9] = [[TileGenerationState::new(); 9]; 9];
+    let mut candidate_board: [TileGenerationState; 81] = [(); 81].map(|_| TileGenerationState::new());
 
-    for i in 0..9 {
-        for j in 0..9 {
-            let tile_generation_state = &mut candidate_board[i][j];
-            println!("Tile state for {} {} {:?}", i, j, tile_generation_state);
-            let index = (rng.next_u32() % tile_generation_state.candidate_count as u32) as usize;
-            let value = tile_generation_state.elect_candidate_at_index(index);
-            board.set_tile(i, j, value);
+    let mut pointer = 0;
+    let mut iterations = 0;
+    while pointer < candidate_board.len() && iterations < 500 {
+        let tile_generation_state = &mut candidate_board[pointer];
+        println!("Filling tile {}", pointer);
+        let (x, y) = pointer_to_x_y(pointer);
+        if tile_generation_state.get_candidates().len() == 0 {
+            candidate_board[pointer] = TileGenerationState::new();
+            pointer -= 1;
+            let (x, y) = pointer_to_x_y(pointer);
+            let current = board.get_tile(x, y).unwrap();
+            candidate_board[pointer].remove_candidate_value(current);
+            // undo the side effects of this assignment
+            for side_effect in &candidate_board[pointer].side_effects {
+                candidate_board[*side_effect].candidates.push(current);
+            }
+            candidate_board[pointer].side_effects.clear();
+            // set current to none
+            board.clear_tile(x, y);
+        } else {
+            let candidates = tile_generation_state.get_candidates();
+            let index = (rng.next_u32() % candidates.len() as u32) as usize;
+            let value = *candidates.get(index).unwrap();
+            board.set_tile(x, y, value);
 
             // remove the selected value from the current row and column
             for k in 0..9 {
-                if k != i {
-                    candidate_board[k][j].remove_candidate_value(value);
+                if k != y {
+                    let removed = candidate_board[x * 9 + k].remove_candidate_value(value);
+                    if removed {
+                        candidate_board[pointer].add_side_effect(x * 9 + k)
+                    }
                 }
-                if k != j {
-                    candidate_board[i][k].remove_candidate_value(value);
+            }
+            for k in 0..9 {
+                if k != x {
+                    let removed = candidate_board[k * 9  + y].remove_candidate_value(value);
+                    if removed {
+                        candidate_board[pointer].add_side_effect(k * 9 + y)
+                    }
                 }
             }
 
             // remove the value from this 9x9 segment
-            let segment_row_start = (i / 3) * 3;
-            let segment_column_start = (j / 3) * 3;
+            let segment_row_start = (x / 3) * 3;
+            let segment_column_start = (y / 3) * 3;
 
-            for k in segment_row_start..segment_row_start + 3 {
-                for l in segment_column_start..segment_column_start + 3 {
-                    if k != i && l != j {
-                        println!("Removing {} from {} {}", value, k, l);
-                        candidate_board[k][l].remove_candidate_value(value);
+            for i in segment_row_start..segment_row_start + 3 {
+                for j in segment_column_start..segment_column_start + 3 {
+                    if i != x && j != y {
+                        println!("Removing {} from {} {}", value, i, j);
+                        let removed = candidate_board[i * 9 + j].remove_candidate_value(value);
+                        if removed {
+                            candidate_board[pointer].add_side_effect(i * 9 + j)
+                        }
                     }
                 }
             }
+            pointer += 1
         }
+        iterations += 1;
     }
-       
+    println!("{:?}", candidate_board);
     board
 }
 
@@ -117,6 +158,7 @@ mod tests {
     #[test]
     fn board_generates() {
         let board = generate_board(GenerationParams::default());
+        board.print();
         assert_eq!(board.validate(), true);
     }
 }
